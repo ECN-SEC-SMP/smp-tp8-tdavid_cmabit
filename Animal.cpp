4ead34e2a353f745473ad3c01cf16dbc7b527417
CharlesSWE <charles.mabit92@gmail.com>
2026-03-10 20:54:06 +0100

    finish Attaque class & fail to finish Animal

--- Animal.cpp
+++ Animal.cpp
@@ -1,14 +1,28 @@
 #include <Animal.h>
 
-
-
 Animal::Animal(int maxX, int maxY)
 {
     this->x = randomMax(maxX-1);
     this->y = randomMax(maxY-1);
 }
 
-void Animal::setAttaque(Attaque atq)
+Animal::Animal(int maxX, int maxY, int a, int b)
 {
+    if (a < maxX){ this->x = a; }
+    if (b < maxY){ this->y = b; }
+}
 
+string Animal::getNom() const { return nom; }
+int Animal::getX() const { return x; }
+int Animal::getY() const { return y; }
+bool Animal::getVivant() const { return vivant; }
+Attaque Animal::getAttaque() const { return typeAttaque; }
+
+void Animal::setVivant(bool v) { this->vivant = v; }
+
+bool Animal::attaque(Animal &a)
+{
+    bool result;
+    result = ;
+    return result;
 }
